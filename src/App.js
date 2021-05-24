d3fcb1705f7ba331c5572a36e5a9a6dd0eb92b8e
Helka Lantto <hmlantto@gmail.com>
2021-05-24 15:01:28 +0300

    Test

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ const App = () => {
 
       <Row>
         <div className="instructions-div" >
-            <p>Autoilumittarilla voit verrata matka-aikaa sekä bensankulutusta eri ajonopeuksilla. Laskuri päivittyy reaaliajassa.</p>
+            <p>Autoilumittarilla voit tarkastella, miten ajonopeuden muutos vaikuttaa matka-aikaan sekä bensankulutukseen. Laskuri päivittyy reaaliajassa.</p>
         </div>
       </Row>
 
